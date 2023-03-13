345130e8bc86acf539e43030e840341dcd0ff7db
Anaga Victor <anagavc@gmail.com>
2023-03-14 00:50:09 +0100

    modified backend api

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://infinite-medicare.herokuapp.com/";
+const BASE_URL = "https://infinite-meidcare.onrender.com/";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
